055acdb4606948e8649df2ea4aeccc208b3e1bee
Ruzanov Sergey <SRuzanov84@gmail.com>
2019-03-18 12:53:17 +0500

    Добавьте файлы проекта.

---
+++ kursovik/Grasshopper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kursovik
+{
+    class Grasshopper
+    {
+        public int CoordinateX { get; set; }
+        public int PrevCoordinateX { get; set; }
+        public void JumpLeft(int sizeJump)
+        {
+            // TODO realization
+        }
+        public void JumpRight(int sizeJump)
+        {
+            // TODO realization
+        }
+    }
+}
