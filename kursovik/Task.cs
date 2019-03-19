da9b3effc46ab8809663e8afe540df6b57995153
Ruzanov Sergey <SRuzanov84@gmail.com>
2019-03-19 22:49:30 +0500

    Add class Task

---
+++ kursovik/Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kursovik
+{
+    class Task
+    {
+        public int StartPosition { get; set; }
+        public int? MaxPosition { get; set; }
+        public int? MinPosition { get; set; }
+        public int SizeLeftJump { get; set; }
+        public int SizeRightJump { get; set; }
+        public int FinishPosition { get; set; }
+        public List<int> PointsPaint { get; set; }
+
+    }
+}
